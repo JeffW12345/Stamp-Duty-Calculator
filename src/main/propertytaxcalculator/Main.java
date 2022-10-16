174cb7901f227b18d5891897a2ff07e9d4a41d2d
Jeff Waters <watersjg@gmail.com>
2022-10-16 09:13:29 +0100

    Added correct web server

--- src/main/propertytaxcalculator/Main.java
+++ src/main/propertytaxcalculator/Main.java
@@ -1,9 +1,22 @@
 package propertytaxcalculator;
+import com.vtence.molecule.Response;
+import com.vtence.molecule.WebServer;
+
+import java.io.IOException;
 
 public class Main {
 
-    public static void main (String[] args) {
-        System.out.println("Hello");
+    public void run(WebServer server) throws IOException {
+        // Start the default web server and provide a single application, which
+        // responds to all incoming requests.
+        server.start(request -> Response.ok().done("Hello, World!"));
+    }
 
+    public static void main(String[] args) throws IOException {
+        Main example = new Main();
+        // Run the default web server
+        WebServer webServer = WebServer.create("127.0.0.1", 8088);
+        example.run(webServer);
+        System.out.println("Access at " + webServer.uri());
     }
 }

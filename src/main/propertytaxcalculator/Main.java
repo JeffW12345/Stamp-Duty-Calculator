a41e922f3ab96ae9cb8d54847c5c76246ecf87bb
Jeff Waters <watersjg@gmail.com>
2022-10-16 17:53:23 +0100

    It finally works!

--- src/main/propertytaxcalculator/Main.java
+++ src/main/propertytaxcalculator/Main.java
@@ -1,18 +1,26 @@
 package propertytaxcalculator;
+
+import com.vtence.molecule.BodyPart;
 import com.vtence.molecule.Response;
 import com.vtence.molecule.WebServer;
+import com.vtence.molecule.lib.TextBody;
 import com.vtence.molecule.routing.Routes;
+import propertytaxcalculator.taxtype.TaxType;
 
 import java.io.IOException;
 
 public class Main {
 
     public void run(WebServer server) throws IOException {
-        server.start(request -> Response.ok().contentType("text/html").done("<!DOCTYPE html>" +
+        server.route(new Routes() {{
+            get("/").to(
+                    request -> Response.ok()
+                            .contentType("text/html; charset=utf-8")
+                            .done("<!DOCTYPE html>" +
                                     "<html>" +
                                     "<body>" +
                                     "<h2>" + "Property tax calculator" + "</h2>" +
-                                    "<form method=\"get\">" +
+                                    "<form enctype='multipart/form-data' action='/' method='post'>\n" +
                                     "<label for=\"property-value\"> Value of property: </label> <br>" +
                                     "<input type=\"text\" id=\"fname\" name=\"property-value\">" + "<br>" +
                                     "<label for=\"tax-type\">" + "Choose a tax type: </label>" +
@@ -26,31 +34,51 @@ public class Main {
                                     "</form>" +
                                     "</body>" +
                                     "</html>"));
-        // Deal with GET request. Takes form http://127.0.0.1:8088/?property-value=234&tax-type=lbbt
-        server.route(new Routes() {{
 
-            get("/?property-value=:property-value&tax-type=:tax-type").to(
-                    request -> Response.ok()
-                            .contentType("text/html")
-                            .done("<html>" +
-                                    "<body>" +
-                                    "<h3>Tax due is " +
-                                    new TaxFactory().create(TaxNames.LBBT,
-                                                    Double.parseDouble(request.parameter("property-value"))).
-                                            getTaxDue() +
-                                    "</body>" +
-                                    "</html>"
-                            ));
+            // A POST to /profile submits the form, then returns a plain text page with a summary of the
+            // profile.
+            post("/").to(request -> {
+                String taxType = request.part("tax-type").value();
+                String propertyValue = request.part("property-value").value();
+                String str = "";
+                if(!isNumeric(propertyValue)){
+                    str += ("You've not given me a number! Press the back button and try again.");
+                }
+                else
+                {
+                    str += "Value of property: " + propertyValue + "\n";
+                    str += "Tax amount: " + taxDue(Double.parseDouble(propertyValue), taxType);
+                }
+                return Response.ok()
+                        .contentType("text/plain; charset=utf-8")
+                        .done(str);
+            });
         }});
+    }
 
-                };
+    private double taxDue(double propertyValue, String taxType) {
+        if(taxType.equals("lbbt")){
+            TaxType tax = new TaxFactory().create(TaxNames.LBBT, propertyValue);
+            tax.calculate();
+            return tax.getTaxDue();
+        }
+        return 0;
+    }
 
-
+    // Stolen from https://stackoverflow.com/questions/1102891/how-to-check-if-a-string-is-numeric-in-java
+    private boolean isNumeric(String name) {
+        try {
+            Double.parseDouble(name);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         Main example = new Main();
         WebServer webServer = WebServer.create("127.0.0.1", 8088);
         example.run(webServer);
-        System.out.println("Access at " + webServer.uri());
+        System.out.println("Access at " + webServer.uri() + "/");
     }
 }

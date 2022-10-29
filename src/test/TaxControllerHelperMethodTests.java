f0400f72c067eb82f8d0c4232b3e612bbc128e35
Jeff Waters <jeff.waters@bjss.com>
2022-10-29 15:25:44 +0100

    Changes to TaxControllerHelperMethodTests

--- src/test/TaxControllerHelperMethodTests.java
+++ src/test/TaxControllerHelperMethodTests.java
@@ -1,21 +1,16 @@
 import main.propertytaxcalculator.controller.TaxController;
 import main.propertytaxcalculator.service.TaxSummaryService;
-import org.junit.Assert;
 import org.junit.Test;
-
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-//import org.springframework.data.util.ReflectionUtils;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TaxControllerHelperMethodTests {
     @Test
     public void isNumericReturnsTrueWithInteger() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        //Method method = TaxController.class.getClass().getDeclaredMethod("isNumeric", String.class);
-        //method.setAccessible(true);
-        //boolean output = (boolean) method.invoke(new TaxController(new TaxSummaryService()), "2");
-        //assertTrue(output);
+        Method method = TaxController.class.getDeclaredMethod("isNumeric", String.class);
+        method.setAccessible(true);
+        TaxController taxController = new TaxController(new TaxSummaryService());
+        assertTrue((boolean) method.invoke(taxController, 2));
     }
 }
